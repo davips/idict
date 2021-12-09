6e712e05ee0dbe823b8f53c958da62e2ffad450a
davips <dpsabc@gmail.com>
2021-12-09 15:11:34 -0300

    Add _name to arff2df

--- src/idict/function/data.py
+++ src/idict/function/data.py
@@ -38,13 +38,13 @@ def df2np(input="df", Xout="X", yout="y", **kwargs):
     {
         "X": "→(input Xout yout df)",
         "y": "→(input Xout yout df)",
-        "_history": "df2np--------pandas-1.3.4--sklearn-1.0.1",
+        "_history": "pandas-1.3.4--sklearn-1.0.1--------df2np",
         "df": "«{'attr1@REAL': {0: 5.1, 1: 3.1}, 'attr2@REAL': {0: 3.5, 1: 4.5}, 'class@{0,1}': {0: '0', 1: '1'}}»",
-        "_id": "ENV9jX8bUi9HrV-xjwOn-33B6zOsklearn-1.0.1",
+        "_id": "cKNOoIVH3tfUBJed.SPVwGXJZmVZ-------df2np",
         "_ids": {
-            "X": "kdwJlaiGhjSUOZuZurFEmzDXV8IiflXdah7r7SZk",
-            "y": "EKjvH0duVbq90SZ2EzjeT6EVQqqCl6eOLc6pR6Tc",
-            "_history": "TUewFCBng1SnVs1OYGlZx7xoaGOFD05axpgd24aw",
+            "X": "lDIdz7jS-Xb39EJBpNjNcX8PXuOPV-H2KU7DnTlI",
+            "y": "xsgM5HvMKa1VkHUZURPFmHUzx5s70M-CjQ6B58fA",
+            "_history": "Hrq7pNr55DDODxNcew3hjRh-9UqR-mklZJf1O2O8",
             "df": "q3_b71eb05c4be05eba7b6ae5a9245d5dd70b81b (content: 6X_dc8ccea3b2e46f1c78967fae98b692701dc99)"
         }
     }
@@ -61,7 +61,7 @@ def df2np(input="df", Xout="X", yout="y", **kwargs):
 
 
 df2np.metadata = {
-    "id": "df2np--------pandas-1.3.4--sklearn-1.0.1",
+    "id": "------pandas-1.3.4--sklearn-1.0.1--df2np",
     "name": "df2np",
     "description": "DataFrame (pandas) to X,y (numpy) converter.",
     "parameters": ...,
@@ -77,11 +77,11 @@ def df2arff(input="df", output="arff", **kwargs):
     >>> d.show(colored=False)
     {
         "a": "→(input output df)",
-        "_history": "df2arff----------------arff2pandas-1.0.1",
+        "_history": "arff2pandas-1.0.1----------------df2arff",
         "df": "«{'attr1@REAL': {0: 5.1, 1: 3.1}, 'attr2@REAL': {0: 3.5, 1: 4.5}, 'class@{0,1}': {0: '0', 1: '1'}}»",
-        "_id": "yBTQeABtgYX1SqQgcAx-ENiKjHdLPerBLwVuTve8",
+        "_id": "NAZxJESCUTEt70sxWcr0wa3dgQl.n33nAmav2Wul",
         "_ids": {
-            "a": "8Bz-W4fdt.XihohXulSzxrOeVd.AKe8Fuq2U.kdr",
+            "a": "Wpz-yOlFhXdoFDT2oWMoXGtdcXWxk3MqjcjUaLtE",
             "_history": "FbwPhhohM9oJ2RiZe6NOVCGxpc5Z-6jYgymCTa1J",
             "df": "q3_b71eb05c4be05eba7b6ae5a9245d5dd70b81b (content: 6X_dc8ccea3b2e46f1c78967fae98b692701dc99)"
         }
@@ -95,7 +95,7 @@ def df2arff(input="df", output="arff", **kwargs):
 
 
 df2arff.metadata = {
-    "id": "df2arff----------------arff2pandas-1.0.1",
+    "id": "--------------arff2pandas-1.0.1--df2arff",
     "name": "df2arff",
     "description": "DataFrame (pandas) to ARFF converter.",
     "parameters": ...,
@@ -141,7 +141,7 @@ def openml(Xout="X", yout="y", name="iris", version=1):
 
 
 openml.metadata = {
-    "id": "openml---------------------sklearn-1.0.1",
+    "id": "-------------------sklearn-1.0.1--openml",
     "name": "openml",
     "description": "Fetch DataFrame+Series (pandas) from OpenML.",
     "parameters": ...,
@@ -162,31 +162,103 @@ def arff2df(input="arff", output="df", **kwargs):
     >>> d.show(colored=False)  # doctest:+ELLIPSIS
     {
         "df": "→(input output arff)",
-        "_history": "arff2df----------------arff2pandas-1.0.1",
+        "_name": "→(input output arff)",
+        "_history": "--------------arff2pandas-1.0.1--arff2df",
         "arff": "@RELATION mini\n@ATTRIBUTE attr1\tREAL\n@ATTRIBUTE attr2 \tREAL\n@ATTRIBUTE class \t{0,1}\n@DATA\n5.1,3.5,0\n3.1,4.5,1",
-        "_id": "...pandas-1.0.1",
+        "_id": "DdTnd2sOCC.9ohqV3hfIRTE5FV3.0.1--arff2df",
         "_ids": {
-            "df": "Cf9JXDY2nGdhA8tup7lik6uD0zSTja4hVl7r7SZk",
-            "_history": "FbwPhhohM9oJ2RiZe6NOVCGxpc5Z-6jYgymCTa1J",
+            "df": "Amuuia7QvqLnnzzID7JTDbqRpqUQX-K1K4AEnTby",
+            "_name": "YD6Zvf2rKoEZwN4N9d5bs83RCbE82M1Cj0zC585q",
+            "_history": "FHG7kK7riNIANfskLtRiJOT3rmmQYmhmZxP.N2Yi",
             "arff": "Z._c3e2b235b697e9734b9ec13084129dc30e45b (content: Ev_8bb973161e5ae900c5743b3c332b4a64d1955)"
         }
     }
+    >>> d.name
+    '<Unnamed>'
     >>> d.df
        attr1@REAL  attr2@REAL class@{0,1}
     0         5.1         3.5           0
     1         3.1         4.5           1
     """
+    relation = "<Unnamed>"
     with StringIO() as f:
         f.write(kwargs[input])
-        df = a2p.loads(f.getvalue())
+        text = f.getvalue()
+        df = a2p.loads(text)
+        for line in text:
+            if line[:9].upper() == "@RELATION":
+                relation = line[9:-1]
+                break
 
-    return {output: df, "_history": ...}
+    return {output: df, "_name": relation, "_history": ...}
 
 
 arff2df.metadata = {
-    "id": "arff2df----------------arff2pandas-1.0.1",
+    "id": "--------------arff2pandas-1.0.1--arff2df",
     "name": "arff2df",
     "description": "ARFF to DataFrame (pandas) converter.",
+    "parameters": ...,
+    "code": ...,
+}
+
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+def nomcols(input="X", output="nomcols", **kwargs):
+    """
+    >>> import numpy as np
+    >>> X = np.array([[0, "a", 1.6], [3.2, "b", 2], [8, "c", 3]])
+    >>> nomcols(X=X)
+    {'nomcols': [1], '_history': Ellipsis}
+    """
+    X = kwargs[input]
+    idxs = []
+    for i, x in enumerate(X[0]):
+        if not is_number(x):
+            idxs.append(i)
+    return {output: idxs, "_history": ...}
+
+
+nomcols.metadata = {
+    "id": "---------------------------------nomcols",
+    "name": "nomcols",
+    "description": "List column indices of nominal attributes.",
+    "parameters": ...,
+    "code": ...,
+}
+
+
+def binarize(input="X", idxsin="nomcols", output="Xbin", **kwargs):
+    """
+    >>> import numpy as np
+    >>> X = np.array([[0, "a", 1.6], [3.2, "b", 2], [8, "c", 3]])
+    >>> binarize(X=X, nomcols=[1])
+    {'Xbin': array([[1. , 0. , 0. , 0. , 1.6],
+           [0. , 1. , 0. , 3.2, 2. ],
+           [0. , 0. , 1. , 8. , 3. ]]), '_history': Ellipsis}
+    """
+    X = kwargs[input]
+    cols = kwargs[idxsin]
+    encoder = OneHotEncoder()
+    nom = encoder.fit_transform(X[:, cols]).toarray()
+    num = np.delete(X, cols, axis=1).astype(float)
+    Xout = np.column_stack((nom, num))
+    return {output: Xout, "_history": ...}
+
+
+binarize.metadata = {
+    "id": "--------------------------------binarize",
+    "name": "binarize",
+    "description": "Binarize nominal attributes so they can be handled as numeric.",
     "parameters": ...,
     "code": ...,
 }
